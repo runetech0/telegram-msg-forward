d72f91c69dcfa012bbb280f9aee4e7d5078de08b
Rehman Ali <rehmanali.9442289@gmail.com>
2020-07-14 18:43:50 +0500

    UPDATE: Implemented configparser config

--- bot.py
+++ bot.py
@@ -2,25 +2,61 @@
 
 from telethon import TelegramClient, events, errors
 # import logging
-import conf
+from configparser import
+
+
+config = ConfigParser()
+config.read('conf.ini')
+
 
 # logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
 # level=logging.WARNING)
 
-api_id = conf.API_ID
-api_hash = conf.API_HASH
-chats = conf.CHANNELS_TO_GET_UPDATES_FROM
-session_file = 'anon'
+api_id = config['TELEGRAM']['api_id']
+api_hash = config['TELEGRAM']['api_hash']
+target_group = config['TELEGRAM'].getint('telegram_destination_group_id')
+session_file = 'telegramBot'
 
 
-client = TelegramClient(session_file, api_id, api_hash)
+# ##############################################< Proxy >##############################################
 
-chats = conf.CHANNELS_TO_GET_UPDATES_FROM
+try:
+    proxy_enabled = config['PROXY'].getboolean('enable')
+    proxy_server = config['PROXY']['server'].encode()
+    proxy_port = config['PROXY'].getint('port')
+except KeyError:
+    proxy_enabled = True
+    proxy_server = '159.89.49.60'
+    proxy_port = 31264
+    pass
+
+
+# if config['proxy']['enable']:
+#     sockProxy = {
+#         "proxy_type": socks.SOCKS5,
+#         "addr": conf.SOCKS5_SERVER,
+#         "port": conf.SOCKS5_PORT,
+#         "rdns": True,
+#         "username": conf.USERNAME,
+#         "password": conf.PASSWORD
+#     }
+
+
+if proxy_enabled:
+    # print(f'Using proxy server {proxy_server}:{proxy_port}')
+    telegramClient = TelegramClient(session_file, api_id, api_hash, proxy=(
+        socks.SOCKS5, proxy_server, proxy_port))
+else:
+    telegramClient = TelegramClient(session_file, api_id, api_hash)
+
+
+chats = conf['TELEGRAM'].['source_channels']
+destination_channel = conf['TELEGRAM'].getint('dest_channel')
 
 
 @client.on(events.NewMessage(chats, blacklist_chats=False))
 async def newMessageHandler(msg):
-    await client.send_message(conf.OWN_CHANNEL_ID, msg.message)
+    await client.send_message(dest_channel, msg.message)
 
 
 try:
